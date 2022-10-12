079c664d4995b202111540e93d3f8d2ede7a5f2c
akshit9179 <akshit.anand@kpipartners.com>
2022-10-12 18:59:32 +0530

    commiting "addition"

---
+++ AdditionJava/src/com/Add.java
@@ -0,0 +1,13 @@
+package com;
+
+public class Add {
+	int addition(int a,int b) {
+		return a+b;
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Add a = new Add();
+		int ans = a.addition(5, 7);
+		System.out.println(ans);
+	}
+}
